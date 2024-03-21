46a10c1b62178b89abebb20c535a18991abfd82e
Bezuro <serfaksion25@gmail.com>
2024-03-21 16:55:13 +0200

    remade migrations

--- database/migrations/2024_03_21_144950_create-csvdata-table.php
+++ database/migrations/2024_03_21_144950_create-csvdata-table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('csvdata', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('email');
-            $table->integer('age');
             $table->dateTime('registration_date');
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('csvdata');
     }
 };
