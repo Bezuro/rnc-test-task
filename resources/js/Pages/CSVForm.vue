92d455789294b09c2f2c0ee280143462b050ef33
Bezuro <serfaksion25@gmail.com>
2024-03-21 17:16:30 +0200

    small fix

--- resources/js/Pages/CSVForm.vue
+++ resources/js/Pages/CSVForm.vue
@@ -78,7 +78,6 @@ const submit = () => {
 
             <div v-if="errors.length > 0">
                 <div
-                    v-if="errors"
                     v-for="(errorMessage, index) in errors"
                     :key="index"
                     class="bg-red-100 border border-red-400 text-red-700 mt-3 px-4 py-3 rounded relative"
